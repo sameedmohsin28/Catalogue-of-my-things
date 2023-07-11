2a5d4617d3e489d1d6d3f1b778a9af428d226bce
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-07-11 21:04:06 +0500

    Add attributes and methods to item class

--- classes/item.rb
+++ classes/item.rb
@@ -1,8 +1,25 @@
 class Item
+  attr_reader :id, :archived
+  attr_accessor :publish_date, :genre, :author_name, :label
   
   def initialize (publish_date, archived)
     @id = Random.rand(1..1000)
     @publish_date = publish_date
     @archived = archived
+    @genre = []
+    @author_name = []
+    @label = []
+  end
+
+  def add_genre(genre)
+    @genre << genre
+  end
+
+  def add_author(author_name)
+    @author_name << author_name
+  end
+
+  def add_label(label)
+    @label << label
   end
 end
