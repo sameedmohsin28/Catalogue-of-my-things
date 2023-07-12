fc7de41e8de30ecc23e9155b308f5c3cdcee95eb
eric <ericopokuantwi384@gmail.com>
2023-07-12 15:33:23 +0000

    finish game class implementation

---
+++ classes/game.rb
@@ -0,0 +1,19 @@
+require './classes/item'
+require 'time'
+
+class Game < Item
+  def initialize(publish_date, archived, last_played_at, multiplayer)
+    super(publish_date, archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    calculated_last_played = ((Time.new - Time.parse(@last_played_at)) / 31_615_673).floor
+    if super && calculated_last_played > 2
+      true
+    else
+      false
+    end
+  end
+end
